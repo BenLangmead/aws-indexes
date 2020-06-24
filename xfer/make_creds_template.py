197b3a7ad7d7cfe8beeed4691b0d15f93912a5eb
Ben Langmead <langmea@cs.jhu.edu>
2020-06-24 14:39:22 -0400

    fix bugs in creds template

--- xfer/make_creds_template.py
+++ xfer/make_creds_template.py
@@ -11,15 +11,18 @@ def make_creds_file(profs, creds_file):
         labels = ['default', 'secondary']
         if len(profs) == 1:
             labels = [labels[0]]
-        for i, label in enumerate(labels):
+        for i, (label, prof) in enumerate(zip(labels, profs)):
             ofh.write('[%s]\n' % label)
+            found = False
             with open(credential_fn, 'rt') as fh:
                 while True:
                     ln = fh.readline()
                     if len(ln) == 0:
                         break
                     ln = ln.rstrip()
-                    if ln[1:-1] == label:
+                    if ln[1:-1] == prof:
+                        found = True
+
                         def _parse(key_to_parse):
                             tokens = fh.readline().rstrip().split('=')
                             ky, vl = tokens[0].strip(), tokens[1].strip()
@@ -27,3 +30,5 @@ def make_creds_file(profs, creds_file):
                             ofh.write('%s = %s\n' % (key_to_parse, vl))
                         _parse('aws_access_key_id')
                         _parse('aws_secret_access_key')
+            if not found:
+                raise RuntimeError('Could not find credentials for profile "%s"' % prof)
