e3d55cc5d014ad937630d0717592c1a7d856907f
Ben Langmead <langmea@cs.jhu.edu>
2020-06-24 14:51:42 -0400

    lowercaseize s3

--- xfer/bt/sync_from_bt2_bench.bash
+++ xfer/bt/sync_from_bt2_bench.bash
@@ -11,6 +11,6 @@ for i in $(aws s3 ls "s3://${SOURCE}/" | awk '{print $NF}') ; do
     echo "Downloading ${i}..."
     aws s3 cp "s3://${SOURCE}/${i}" .
     echo "  Uploading ${i}..."
-    aws --profile secondary s3 cp "${i}" "S3://${DEST}/${i}"
+    aws --profile secondary s3 cp "${i}" "s3://${DEST}/${i}"
     rm -f "${i}"
 done
