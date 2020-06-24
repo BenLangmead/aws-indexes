becfdec363f85df1fce2b1e2e6cc111bedac1439
Ben Langmead <langmea@cs.jhu.edu>
2020-06-24 15:21:50 -0400

    kraken file uploader

---
+++ xfer/kraken/cp_files.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+cd /work
+
+BUCKET="${1}"
+
+while read -r src dst ; do
+    bn=$(basename "${src}")
+    echo "Downloading ${bn}..."
+    wget --quiet "${src}"
+    echo "  Uploading ${bn}..."
+    aws --profile secondary s3 cp --quiet "${bn}" "s3://${BUCKET}/${dst}"
+    rm -f "${bn}"
+done < kraken.manifest
