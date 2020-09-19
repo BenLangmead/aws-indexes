8ea9c55a59f109e8533fd5a57f4fe9ef616022e3
Ben Langmead <langmea@cs.jhu.edu>
2020-09-19 07:38:24 -0400

    add manifest making script

---
+++ xfer/kraken/k2_s3_xfer_manifest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+"""
+E.g.:
+python k2_s3_xfer_manifest.py 20200919
+"""
+
+import sys
+
+
+date = sys.argv[1]
+bracken_lengths = [50, 75, 100, 150, 200, 250, 300]
+files = ['hash.k2d', 'opts.k2d', 'taxo.k2d', 'seqid2taxid.map']
+
+for ln in bracken_lengths:
+    files.append('database%dmers.kmer_distrib' % ln)
+
+dbs = ['minusb',
+       'standard', 'standard_8gb', 'standard_16gb',
+       'pluspf', 'pluspf_8gb', 'pluspf_16gb',
+       'pluspfp', 'pluspfp_8gb', 'pluspfp_16gb']
+
+source_url = 's3://k2-dbs/dbs'
+dest_path = 'kraken'
+
+sources = list(map(lambda x: source_url + '/mkidx_' + x, dbs))
+dests = list(map(lambda x: dest_path + '/' + x + '_' + date, dbs))
+archive_fns = list(map(lambda x: dest_path + '/k2_' + x + '_' + date, dbs))
+
+for src, dst, archive_fn in zip(sources, dests, archive_fns):
+    for file in files:
+        print('%s/%s %s/%s' % (src, file, dst, file))
+    print('%s/db.tar.gz %s' % (src, archive_fn))
