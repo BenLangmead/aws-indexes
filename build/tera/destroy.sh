bf3a8596e3840571ad23fbb2a1725aa566372002
Benjamin Langmead <langmea@cs.jhu.edu>
2026-04-01 14:34:12 -0400

    Fewer warnings from destroy script

--- build/tera/destroy.sh
+++ build/tera/destroy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-npx aws-cdk destroy
+# Silence JSII untested Node version warning and Node deprecation warnings (e.g. url.parse)
+export JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1
+export NODE_OPTIONS="${NODE_OPTIONS:+$NODE_OPTIONS }--no-deprecation"
+npx aws-cdk destroy --force
